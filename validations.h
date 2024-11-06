7170f4fe258eb0a371dd249f33a87b456a4c1716
kenny <badnight0903@gmail.com>
2024-11-06 11:18:02 +0100

    Update validations.h

--- validations.h
+++ validations.h
@@ -2,9 +2,10 @@
 // Created by User on 29.10.2024.
 //
 
-#ifndef VALIDATIONS_H
-#define VALIDATIONS_H
+#ifndef VALIDATION_H
+#define VALIDATION_H
 #include <stdio.h>
+#include <stdbool.h>
 #include <windows.h>
 
 void setTextColor(int color)
@@ -13,95 +14,106 @@ void setTextColor(int color)
     SetConsoleTextAttribute(hConsole, color);
 }
 
-static int validate_integer_value(const char *message)
+static int validate_integer_limits_value(const char *message)
 {
+    bool InvalidInput;
     int value = 0;
     do
     {
         printf("%s\n", message);
+        fflush(stdin);
+        if (scanf("%d", &value) != 1 || value < -360 || value > 360)
+        {
+            setTextColor(12);
+            printf("Invalid Input. Enter an integer value in given range\n");
+            setTextColor(7);
+            fflush(stdin);
+            InvalidInput = true;
+        }
+        else
+        {
+            fflush(stdin);
+            InvalidInput = false;
+        }
+    }
+    while(InvalidInput);
+    return value;
+}
+static int validate_integer_step_value(const char *message)
+{
+    bool InvalidInput;
+    int value = 0;
+    do
+    {
+        printf("%s\n", message);
+        fflush(stdin);
         if (scanf("%d", &value) != 1)
         {
             setTextColor(12);
             printf("Invalid Input. Enter an integer value\n");
             setTextColor(7);
             fflush(stdin);
+            InvalidInput = true;
         }
         else
         {
             fflush(stdin);
-            break;
+            InvalidInput = false;
         }
     }
-    while(value != 1);
-    return value;
-}
-
-static double validate_floating_value(const char *message)
-{
-    double value = 0;
-    do
-    {
-        printf("%s\n", message);
-        if (scanf("%lf", &value) != 1)
-        {
-            setTextColor(12);
-            printf("Invalid Input. Enter a real number\n");
-            setTextColor(7);
-            fflush(stdin);
-        }
-        else
-        {
-            fflush(stdin);
-            break;
-        }
-    }
-    while(value != 1);
+    while(InvalidInput);
     return value;
 }
 
 static int validate_integer_accuracy(const char *message)
 {
+    bool InvalidInput;
     int value = 0;
-    printf("%s\n", message);
+    printf("%s", message);
+    fflush(stdin);
     do
     {
-        if (scanf("%d", &value) != 1 || value <= 0)
+        if (scanf("%d", &value) != 1 || value <= 0 || value > 15)
         {
             setTextColor(12);
-            printf("Invalid Input, enter an integer number greater than zero\n");
+            printf("Invalid Input, please enter an integer number in given range\n");
             fflush(stdin);
             setTextColor(7);
+            InvalidInput = true;
         }
         else
         {
             fflush(stdin);
-            break;
+            InvalidInput = false;
         }
     }
-    while(value != 1 || value <= 0);
+    while(InvalidInput);
     return value;
 }
 
 static double validate_floating_accuracy(const char *message1, const char *message2)
 {
+    bool InvalidInput;
     double value = 0;
     printf("%s\n", message1);
+    fflush(stdin);
     do
     {
-        if (scanf("%lf", &value) != 1 || value <= 0)
+        if (scanf("%lf", &value) != 1 || value <= 0 || value < 1e-15 || value > 1e-1)
         {
             setTextColor(12);
             printf("%s\n", message2);
             setTextColor(7);
             fflush(stdin);
+            InvalidInput = true;
         }
         else
         {
             fflush(stdin);
-            break;
+            InvalidInput = false;
         }
     }
-    while(value != 1 || value <= 0);
+    while(InvalidInput);
     return value;
 }
-#endif //VALIDATIONS_H
+#endif //VALIDATION_H
