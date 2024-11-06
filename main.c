e34e05aa58c1c3c4956634cce666eebf39478d2c
kenny <badnight0903@gmail.com>
2024-11-06 11:17:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,35 +1,50 @@
 #include <stdio.h>
 #include <conio.h>
-#include "functions.h"
-#include "calculations.h"
-#include "validations.h"
-
-#define MAX_SIZE 100
+#include "function.h"
+#include "calculation.h"
+#include "validation.h"
 
 int main()
 {
     int x1 = 0, x2 = 0, dx = 0;
-    double e = 0, x = 0;
-    double x_values[MAX_SIZE], taylor_values[MAX_SIZE], functions_values[MAX_SIZE], differences[MAX_SIZE];
     int count = 0;
+    double x = 0, e = 0;
     const char*  header1 = 0;
     const char*  header2 = 0;
+    const char*  header3 = 0;
+    const char*  header4 = 0;
     do
     {
-        setTextColor(2);
         printf("This program calculates taylor series of sine/cosine, it's table value by standard functions, and their difference\n");
-        setTextColor(7);
         count = 0;
         header1 = NULL;
         header2 = NULL;
+        header3 = NULL;
+        header4 = NULL;
 
-        x1 = validate_integer_value("Enter x1 (degrees):");
-        x2 = validate_integer_value("Enter x2 (degrees):");
+        bool InvalidInput;
+        do
+        {
+
+            x1 = validate_integer_limits_value("Enter x1 (degrees in range [-360, 360]):");
+            x2 = validate_integer_limits_value("Enter x2 (degrees in range [-360, 360]):");
+            InvalidInput = x1 == x2
+                    ? (setTextColor(12), printf("The values can't be equal. Otherwise the program will break\n"), setTextColor(7), fflush(stdin), true)
+                    : false;
+        }
+        while(InvalidInput);
+            x1 > x2
+                    ? (printf("The step can't be higher than: "), setTextColor(2), printf("%d\n", x1 - x2), setTextColor(7))
+                        : (printf("The step can't be higher than: "), setTextColor(2), printf("%d\n", x2 - x1), setTextColor(7));
 
         dx = dx_val(x1, x2);
+
+        const int size = (x2 - x1) / dx + 1;
+        double x_values[size], taylor_values[size], table_value[size], differences[size];
+
         e = get_accuracy();
 
-        int choice = 0;
+        int choice;
         do
         {
             printf("What do you want to calculate?\n");
@@ -44,41 +59,50 @@ int main()
         }
         while (choice != 49 && choice != 50);
 
-        for (x = (double)x1; x <= (double)x2 && count < MAX_SIZE; x += (double)dx)
+        header1 = "x (degrees)";
+        header4 = "Difference";
+
+        x = x1;
+        do
         {
             double taylor_series = 0, functions_result = 0;
             const double radians = degrees_to_radians(x);
             if (choice == 49)
             {
-                header1 = "Taylor series cosine";
-                header2 = "Functions cosine";
+                header2 = "Taylor series cosine";
+                header3 = "Table cosine";
                 functions_result = cos(radians);
                 taylor_series = Taylor_cos(radians, e);
             }
-            else if (choice == 50)
+            else
+                if (choice == 50)
             {
-                header1 = "Taylor series sine";
-                header2 = "Functions sine";
+                header2 = "Taylor series sine";
+                header3 = "Table sine";
                 functions_result = sin(radians);
                 taylor_series = Taylor_sin(radians, e);
             }
             x_values[count] = x;
             taylor_values[count] = taylor_series;
-            functions_values[count] = functions_result;
+            table_value[count] = functions_result;
             differences[count] = functions_result - taylor_series;
             count++;
+            x += (double)dx;
         }
-        printf("\n|   x (degrees)   |   %s   |   %s   |   Difference   |\n", header1, header2);
-        printf("-------------------------------------------------------------------------------------\n");
+        while (dx > 0 && x <= x2 || dx < 0 && x >= x2 && count < size);
+        printf("\n|   %-15s   |   %-20s   |   %-20s   |   %-20s|\n", header1, header2, header3, header4);
+        printf("-----------------------------------------------------------------------------------------------------\n");
+
 
         for (int i = 0; i < count; i++)
         {
-            printf("| %e | %e | %e | %e |\n",
-                   x_values[i], taylor_values[i], functions_values[i], differences[i]);
+            printf("|   %-15.2f   |   %-20e   |   %-20e   |   %-20e|\n",
+                   x_values[i], taylor_values[i], table_value[i], differences[i]);
         }
         setTextColor(2);
         printf("To restart the program press ENTER, otherwise press any key to exit\n");
         setTextColor(7);
+        while (getchar() != '\n');
     }
     while(getch() == 13);
     return 0;
